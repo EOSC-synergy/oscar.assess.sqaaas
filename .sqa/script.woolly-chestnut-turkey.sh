2b8d51813636430a9b9c46131949948f29a78a4a
Pablo Orviz <orviz@ifca.unican.es>
2025-01-30 11:29:22 +0100

    Add JePL file structure

--- .sqa/script.woolly-chestnut-turkey.sh
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
-# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
-#
-# SPDX-License-Identifier: GPL-3.0-only
-
-(
-cd github.com/grycap/oscar &&
-    hadolint Dockerfile examples/radiomics/Dockerfile examples/fire-detection/Dockerfile examples/text-to-speech-google/Dockerfile examples/plant-classification-theano/Dockerfile examples/text-to-speech-coqui/Dockerfile examples/cowsay/Dockerfile examples/stable-diffusion/Dockerfile examples/compss/java/Dockerfile examples/compss/c/Dockerfile examples/compss/python/Dockerfile tools/oscar-bin-downloader/Dockerfile --failure-threshold error
-)
