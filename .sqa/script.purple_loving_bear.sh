a6be20b01958bbb9451b995d1fcb260b131ccfd6
samuelbernardolip <44866834+samuelbernardolip@users.noreply.github.com>
2026-06-10 12:09:55 +0100

    Add JePL file structure

---
+++ .sqa/script.purple_loving_bear.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/grycap/oscar &&
+    hadolint Dockerfile examples/cowsay/Dockerfile examples/fire-detection/Dockerfile examples/kserve-isvc-yolo/Dockerfile examples/plant-classification-theano/Dockerfile examples/radiomics/Dockerfile examples/stable-diffusion/Dockerfile examples/text-to-speech-coqui/Dockerfile examples/text-to-speech-google/Dockerfile examples/vllm-deepseek/Dockerfile examples/vllm-llama/Dockerfile examples/compss/c/Dockerfile examples/compss/java/Dockerfile examples/compss/python/Dockerfile tools/oscar-bin-downloader/Dockerfile --failure-threshold error
+)
